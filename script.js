b00c306854a9ebd0a7faf492646a68373b83beca
Santi Garcia <58360969+santosuke@users.noreply.github.com>
2021-01-10 14:35:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const counter = document.getElementById("counter");
+// const content = document.querySelector(".content");
+// const projects = document.querySelector("#projects");
+
+// // const logScroll = () => {
+
+// // };
+
+// window.addEventListener("scroll", function () {
+//   // let scrollPostition = Math.floor(window.pageYOffset);
+//   let projectsScroll = Math.floor(projects.offsetTop);
+//   // console.log(scrollPostition);
+//   counter.innerHTML = projectsScroll;
+// });
